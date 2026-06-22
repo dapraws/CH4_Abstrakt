06a9687f984aba4391fe80c0bf0c94a1de8cd426
dapraws <darrelprawira26@gmail.com>
2026-06-22 19:54:55 +0800

    Initial Commit

---
+++ Abstrakt/AbstraktApp.swift
@@ -0,0 +1,17 @@
+//
+//  AbstraktApp.swift
+//  Abstrakt
+//
+//  Created by Muhammad Darrel Prawira on 22/06/26.
+//
+
+import SwiftUI
+
+@main
+struct AbstraktApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
